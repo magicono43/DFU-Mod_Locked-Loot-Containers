f91b969b25faeabd224d2216ecdc8a2c4c2049e9
magicono43 <39843717+magicono43@users.noreply.github.com>
2022-09-11 16:44:58 -0400

    Initial Commit, 9/11/2022
    Initial Commit, 9/11/2022.

---
+++ Scripts/LockedLootContainersMain.cs
@@ -0,0 +1,46 @@
+// Project:         LockedLootContainers mod for Daggerfall Unity (http://www.dfworkshop.net)
+// Copyright:       Copyright (C) 2022 Kirk.O
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Author:          Kirk.O
+// Created On: 	    9/8/2022, 11:00 PM
+// Last Edit:		9/8/2022, 11:00 PM
+// Version:			1.00
+// Special Thanks:  
+// Modifier:			
+
+using UnityEngine;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+
+namespace LockedLootContainers
+{
+    public class LockedLootContainersMain : MonoBehaviour
+    {
+        static LockedLootContainersMain instance;
+
+        public static LockedLootContainersMain Instance
+        {
+            get { return instance ?? (instance = FindObjectOfType<LockedLootContainersMain>()); }
+        }
+
+        static Mod mod;
+
+        [Invoke(StateManager.StateTypes.Start, 0)]
+        public static void Init(InitParams initParams)
+        {
+            mod = initParams.Mod;
+            instance = new GameObject("LockedLootContainers").AddComponent<LockedLootContainersMain>(); // Add script to the scene.
+
+            mod.IsReady = true;
+        }
+
+        private void Start()
+        {
+            Debug.Log("Begin mod init: Locked Loot Containers");
+
+            // Do stuff
+
+            Debug.Log("Finished mod init: Locked Loot Containers");
+        }
+    }
+}
