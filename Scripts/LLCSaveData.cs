f8cf522849096d0124c0bbaa3d82b926baeec642
magicono43 <39843717+magicono43@users.noreply.github.com>
2023-03-02 23:42:55 -0500

    Did Very Small Amount More Work On Save Data Stuff, 3/2/2023
    Did Very Small Amount More Work On Save Data Stuff. Hopefully getting somewhat more of a grasp on how I'm going to do this for now, 3/2/2023.

--- Scripts/LLCSaveData.cs
+++ Scripts/LLCSaveData.cs
@@ -10,13 +10,45 @@ using DaggerfallWorkshop.Game.MagicAndEffects;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
 using System;
 using System.Collections.Generic;
+using DaggerfallWorkshop.Game.Serialization;
 
 namespace LockedLootContainers
 {
     [FullSerializer.fsObject("v1")]
+    public class ChestData_v1
+    {
+        public ulong loadID;
+        public int[] recentInspectValues;
+        public bool isLockJammed;
+        public bool hasBeenBashed;
+        public bool hasBeenInspected;
+        public ChestMaterials chestMaterial;
+        public int chestSturdiness;
+        public int chestMagicResist;
+        public LockMaterials lockMaterial;
+        public int lockSturdiness;
+        public int lockMagicResist;
+        public int lockComplexity;
+        public int jamResist;
+        public int picksAttempted;
+        public int lockBashedLightTimes;
+        public int lockBashedHardTimes;
+        public int chestBashedLightTimes;
+        public int chestBashedHardTimes;
+        public Vector3 currentPosition;
+        public Vector3 localPosition;
+        public ItemData_v1[] attachedLoot;
+    }
+
+    [FullSerializer.fsObject("v1")]
+    public struct ChestsSaveData_v1
+    {
+        public ChestData_v1[] chests;
+    }
+
     public class LLCSaveData : IHasModSaveData
     {
-        public Dictionary<string, List<LandmarkLocation>> Towns; // Continue work on renovating this Penwick Papers save-data tomorrow for my chest objects in a dictionary or something.
+        public Dictionary<string, List<LandmarkLocation>> Towns; // Continue work on this tomorrow, this time likely using mostly example from Kab's "Location Loader" mod for WoD and such.
         public List<LandmarkLocation> DungeonLocations;
 
         public LLCSaveData()
